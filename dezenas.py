9a61ff0520c354cd457325aebc6896407205d044
Jadsonsr <68830979+Jadsonsr@users.noreply.github.com>
2020-08-15 22:31:23 -0300

    Add files via upload

---
+++ dezenas.py
@@ -0,0 +1,9 @@
+dg1 = input("Digite um número inteiro: ")
+d1 = int(dg1)
+dd1 = ((d1 % 100) // 10)
+print("O dígito das dezenas é", dd1)
+
+dg2 = input("Digite um número inteiro: ")
+d2 = int(dg2)
+dd2 = ((d2 % 100) // 10)
+print("O dígito das dezenas é", dd2)
